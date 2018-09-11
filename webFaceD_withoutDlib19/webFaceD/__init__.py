29de21e1ad8be7e438ca1b190100cbfd2eb22cf4
bio-punk <cory@hnu.edu.cn>
2018-09-11 14:08:03 +0800

    20180911-02
    Signed-off-by: bio-punk <cory@hnu.edu.cn>

--- webFaceD_withoutDlib19/webFaceD/__init__.py
+++ webFaceD_withoutDlib19/webFaceD/__init__.py
@@ -11,6 +11,8 @@ import cv2
 import dlib
 
 detector=dlib.get_frontal_face_detector()
+shape_predictor_5p = dlib.shape_predictor("shape_predictor_5_face_landmarks.dat")
+shape_predictor_68p = dlib.shape_predictor("shape_predictor_68_face_landmarks.dat")
 # cnn_detector = dlib.cnn_face_detection_model_v1("mmod_human_face_detector.dat")
 
 import webFaceD.api
--- webFaceD_withoutDlib19/webFaceD/api/face/__init__.py
+++ webFaceD_withoutDlib19/webFaceD/api/face/__init__.py
@@ -1,4 +1,6 @@
 from .Detect import *
 # from .CnnDetect import *
+from .Landmarks5p import *
+from .Landmarks68p import *
 
 import webFaceD.api.face.views
